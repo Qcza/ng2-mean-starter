54d9aa4897b80bd7840c921451ed62f42df8b181
pku <pawel.kuczynski@ue.wroc.pl>
2016-09-07 13:17:30 +0200

    1.0.0

--- spec/server.spec.ts
+++ spec/server.spec.ts
@@ -1,1 +1,13 @@
 import * as request from 'request';
+
+const port = 3000;
+const main_url = `http://localhost:${port}`;
+
+describe("Landing page", function() {
+  it("should return status code 200", function(done) {
+    request(main_url, function (err, res, body) {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+});
