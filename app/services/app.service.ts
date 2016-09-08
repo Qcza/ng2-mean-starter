997b8fc84b6a155427973559199195d597f47756
pku <pawel.kuczynski@ue.wroc.pl>
2016-09-08 10:47:26 +0200

    1.0.0

--- app/services/app.service.ts
+++ app/services/app.service.ts
@@ -19,27 +19,27 @@ export class AppService {
   //   return this.http.get(this.customUrl , options).map(this.extractData).catch(this.handleError);
   // }
 
-  // getTemplate (id:string): Observable<any> {
+  // getCustoms (id:string): Observable<any> {
   //   let headers = new Headers({'Auth': 'basic'});
   //   let options = new RequestOptions({headers: headers});
   //   return this.http.get(this.customUrl  + '/' +id, options).map(this.extractData).catch(this.handleError);
   // }
 
-  // postTemplate (custom:any): Observable<any> {
+  // postCustom (custom:any): Observable<any> {
   //   let body = JSON.stringify(custom);
   //   let headers = new Headers({'Content-type': 'application/json', 'Auth': 'basic'});
   //   let options = new RequestOptions({headers: headers});
   //   return this.http.post(this.customUrl, body, options).map(this.extractData).catch(this.handleError);
   // }
 
-  // putTemplate (custom:any): Observable<any> {
+  // putCustom (custom:any): Observable<any> {
   //   let body = JSON.stringify(custom);
   //   let headers = new Headers({'Content-type': 'application/json', 'Auth': 'basic'});
   //   let options = new RequestOptions({headers: headers});
   //   return this.http.put(this.customUrl+'/'+custom.id, body, options).map(this.extractData).catch(this.handleError);
   // }
 
-  // deleteTemplate (custom:any): Observable<any> {
+  // deleteCustom(custom:any): Observable<any> {
   //   let headers = new Headers({'Auth': 'basic'});
   //   let options = new RequestOptions({headers: headers});
   //   return this.http.delete(this.customUrl+'/'+custom.id, options).map(this.extractData).catch(this.handleError);
