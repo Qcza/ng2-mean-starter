fc1f0d80bf9056f8cd5cfd30ab1e03ea8a9160c4
pku <pawel.kuczynski@ue.wroc.pl>
2016-09-12 12:05:54 +0200

    1.0.0

--- spec/server.spec.ts
+++ spec/server.spec.ts
@@ -3,6 +3,11 @@ import * as assert from 'assert';
 import * as mongodb from 'mongodb';
 import * as config from '../config';
 
+// MONGO CONFIG
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectID;
+const dbUrl = (config.dbUser && config.dbPwd) ? ('mongodb://' + config.dbUser + ':' + config.dbPwd + '@' + config.dbUrl) : ('mongodb://' + config.dbUrl);
+
 const port = config.port;
 const main_url = `http://localhost:${port}`;
 
